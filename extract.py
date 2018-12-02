216847fe40b0949e8d6b819d7057c1cd5ce70962
ramazanyersainov <ramazan.yersainov@nu.edu.kz>
2018-12-02 16:33:20 +0600

    extract2

---
+++ extract.py
@@ -0,0 +1,39 @@
+import io
+
+from pdfminer.converter import TextConverter
+from pdfminer.pdfinterp import PDFPageInterpreter
+from pdfminer.pdfinterp import PDFResourceManager
+from pdfminer.pdfpage import PDFPage
+from string import digits
+
+
+def extract_text_from_pdf(pdf_path):
+    resource_manager = PDFResourceManager()
+    fake_file_handle = io.StringIO()
+    converter = TextConverter(resource_manager, fake_file_handle)
+    page_interpreter = PDFPageInterpreter(resource_manager, converter)
+
+    with open(pdf_path, 'rb') as fh:
+        for page in PDFPage.get_pages(fh,
+                                      caching=True,
+                                      check_extractable=True):
+            page_interpreter.process_page(page)
+
+        text = fake_file_handle.getvalue()
+
+    # close open handles
+    converter.close()
+    fake_file_handle.close()
+
+    if text:
+        return text
+
+
+if __name__ == '__main__':
+    wholefile = extract_text_from_pdf('sst.pdf')
+    i = wholefile.index("Room")
+    wholefile = wholefile[i+4:]
+    arr = [ x.strip() for x in wholefile.split('cap:') ]
+    for i in range(0,len(arr)):
+        arr[i]=arr[i].lstrip(digits)
+    print(arr)
